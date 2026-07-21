9fc3856ab70cac8ea1eb240caa011daef254714a
pariharabhishek135-sudo <pariharabhishek135@gmail.com>
2026-07-21 18:44:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 st.title("Customer Churn Prediction")
 st.success("Streamlit is Working!")
+import streamlit as st
+import joblib
+
+st.title("Customer Churn Prediction")
+
+try:
+    model = joblib.load("model.pkl")
+    st.success("✅ Model Loaded Successfully")
+except Exception as e:
+    st.error("Model Loading Failed")
+    st.exception(e)
