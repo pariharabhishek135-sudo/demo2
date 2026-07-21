ca4447eaa2fe513f48acef3b9718d5bf541af869
pariharabhishek135-sudo <pariharabhishek135@gmail.com>
2026-07-21 18:36:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,4 @@
 import streamlit as st
-import pandas as pd
-import joblib
-
-st.set_page_config(page_title="Customer Churn Prediction")
 
 st.title("Customer Churn Prediction")
-
-# Load Model
-model = joblib.load("model.pkl")
-
-# Inputs
-CreditScore = st.number_input("Credit Score", min_value=300, max_value=900, value=600)
-Geography = st.selectbox("Geography", ["France", "Germany", "Spain"])
-Gender = st.selectbox("Gender", ["Male", "Female"])
-Age = st.number_input("Age", min_value=18, max_value=100, value=30)
-Tenure = st.number_input("Tenure", min_value=0, max_value=10, value=5)
-Balance = st.number_input("Balance", value=0.0)
-NumOfProducts = st.number_input("Number of Products", min_value=1, max_value=4, value=1)
-HasCrCard = st.selectbox("Has Credit Card", [0, 1])
-IsActiveMember = st.selectbox("Is Active Member", [0, 1])
-EstimatedSalary = st.number_input("Estimated Salary", value=50000.0)
-
-if st.button("Predict"):
-
-    data = pd.DataFrame({
-        "CreditScore": [CreditScore],
-        "Geography": [Geography],
-        "Gender": [Gender],
-        "Age": [Age],
-        "Tenure": [Tenure],
-        "Balance": [Balance],
-        "NumOfProducts": [NumOfProducts],
-        "HasCrCard": [HasCrCard],
-        "IsActiveMember": [IsActiveMember],
-        "EstimatedSalary": [EstimatedSalary]
-    })
-
-    prediction = model.predict(data)
-
-    if prediction[0] == 1:
-        st.error("Customer Will Churn")
-    else:
-        st.success("Customer Will Not Churn")
+st.success("Streamlit is Working!")
