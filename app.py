a94406336bb18ac990a5de94f126411fb85c7016
pariharabhishek135-sudo <pariharabhishek135@gmail.com>
2026-07-21 22:37:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,3 +25,75 @@ if st.button('Predict'):
         st.progress(int(p*100))
         st.write(f'Churn Probability: {p*100:.2f}%')
     st.success('Customer Will Not Churn' if pred==0 else 'Customer Will Churn')
+    from PIL import Image
+
+logo = Image.open("bank_logo.png")
+
+col1, col2 = st.columns([1,5])
+
+with col1:
+    st.image(logo, width=90)
+
+with col2:
+    st.markdown("""
+    # 🏦 Customer Churn Prediction
+    ### AI Powered Banking Analytics
+    """)
+    st.markdown("---")
+
+st.markdown("""
+<div style="
+background-color:#0F172A;
+padding:15px;
+border-radius:10px;
+text-align:center;
+color:white;
+">
+<h4>👨‍💻 Developed by Abhishek Parihar</h4>
+<p>B.Tech | Artificial Intelligence & Machine Learning</p>
+<p>Customer Churn Prediction System using Machine Learning</p>
+</div>
+""", unsafe_allow_html=True)
+st.markdown("## 📊 Dashboard Overview")
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.metric(
+        label="👥 Customers",
+        value="10,000",
+        delta="+250"
+    )
+
+with col2:
+    st.metric(
+        label="📉 Churn Rate",
+        value="20.4%",
+        delta="-1.2%"
+    )
+
+with col3:
+    st.metric(
+        label="💳 Active Members",
+        value="5,151",
+        delta="+98"
+    )
+
+with col4:
+    st.metric(
+        label="💰 Avg Balance",
+        value="$76,485",
+        delta="+3.4%"
+    )
+
+st.markdown("---")
+if prediction[0] == 1:
+    st.metric(
+        "Risk Level",
+        "🔴 HIGH"
+    )
+else:
+    st.metric(
+        "Risk Level",
+        "🟢 LOW"
+    )
